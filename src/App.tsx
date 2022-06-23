5a42b576ae2a5d6d2f85761fa92248bd04bdf2fd
nomadHash <badsoap141@gmail.com>
2022-06-23 11:21:59 +0900

    add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import { ReactElement } from 'react';
 
-const App = () => {
-  return <div>App</div>;
+const App = (): ReactElement => {
+    return <div>Hello World!</div>;
 };
 
 export default App;
